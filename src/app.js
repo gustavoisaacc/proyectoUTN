d816f468728e7488abd6ac6d2f51a5cf82088fea
gustavoisaacc <gustavochaile7@gmail.com>
2024-05-29 07:51:17 -0300

    backend environment configuration

--- src/app.js
+++ src/app.js
@@ -1,4 +1,18 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 export const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+// Routes
+
+// Error handling
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    status: "error",
+    message: err.message,
+  });
+});
