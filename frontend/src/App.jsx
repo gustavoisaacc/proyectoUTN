075990389a04719cb999004af2e7ffe80d0473ac
RacedoSebastian <racedosebas@gmail.com>
2024-05-30 15:19:13 -0300

    agregando componentes sueltos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import Boton from "./components/boton";
+import P from "./components/ui/p";
+
 function App() {
   return (
     <div className="bg-black min-h-screen grid place-content-center">
       <h1 className="text-white">Hello, world!</h1>
+      <div>
+        <Boton name="abrir" />
+      </div>
+      <P name="hamburguesa con descuento"/>
     </div>
   );
 }
---
+++ frontend/src/components/ui/p.jsx
@@ -0,0 +1,7 @@
+const P = (props) =>{
+    return(
+        <p className="font-sans text-xl">{props.name} </p>
+    )
+} 
+
+export default P
