58b84e0d16798f64dea69283860fb75dcd3976e3
RacedoSebastian <racedosebas@gmail.com>
2024-05-30 15:48:48 -0300

    creacion del componente card head

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,11 @@
-import Boton from "./components/boton";
-import P from "./components/ui/p";
+import CardHeders from "./components/cardHeders";
 
 function App() {
   return (
-    <div className="bg-black min-h-screen grid place-content-center">
-      <h1 className="text-white">Hello, world!</h1>
-      <div>
-        <Boton name="abrir" />
-      </div>
-      <P name="hamburguesa con descuento"/>
-    </div>
+    <>
+      <CardHeders />
+    </>
+
   );
 }
 
---
+++ frontend/src/components/cardHeders.jsx
@@ -0,0 +1,17 @@
+import P from "./ui/p.jsx"
+import Boton from "./ui/boton.jsx"
+
+
+const CardHeders = () => {
+    return (
+        <>
+            <h1>BIENVENIDOS</h1>
+            <P name="Presione el boton para entrar"/>
+            <Boton name="Entrar"/>
+        </>
+
+
+    )
+}
+
+export default CardHeders
