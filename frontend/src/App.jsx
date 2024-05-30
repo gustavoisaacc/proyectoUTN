8e6ec06e22876c60005d70b7486f23a76139e96c
gustavoisaacc <gustavochaile7@gmail.com>
2024-05-30 13:50:06 -0300

    add the schemas product and categories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
+import Button from "./Button";
+
 function App() {
   return (
     <div className="bg-black min-h-screen grid place-content-center">
       <h1 className="text-white">Hello, world!</h1>
+      <Button name="enviar" className="bg-primary text-white"/>
+      <Button name="comprar" className="bg-red-500 text-white"/>
     </div>
   );
 }
