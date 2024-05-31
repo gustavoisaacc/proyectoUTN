b169c6944e0dbaf68151b2bf99d291aa9bc4e464
RacedoSebastian <racedosebas@gmail.com>
2024-05-31 20:13:19 -0300

    agregando footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,6 @@
-import CardHeders from "./components/cardHeders";
-
 function App() {
   return (
     <>
-      <CardHeders />
     </>
 
   );
---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,16 @@
+import P from "./ui/p"
+
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="flex justify-around bg-primary ">
+                <P name="UBICACION" />
+                <P name="REDES" />
+                <P name="COPYRIGT" />
+            </footer>
+        </>
+    )
+}
+
+export default Footer
