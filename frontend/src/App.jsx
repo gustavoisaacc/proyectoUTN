130bc04c065357ddbfddc232679b078eb1053673
melcato <catoherrera17@hotmail.com>
2024-05-31 23:37:31 -0300

    modifies one button component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
-import Button from "./components/ui/Button";
+import Button from "./components/ui/ButtonMel";
 
 function App() {
   return (
     <div className="bg-black min-h-screen grid place-content-center">
       <h1 className="text-white">Hello, world!</h1>
-     <Button name="Bienvenidos" className="bg-blue-500 text-white"/>
-     <Button name="Roniosos" className="text-blue-500"/>
+     
      
     </div>
 
