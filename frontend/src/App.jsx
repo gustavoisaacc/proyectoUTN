4e4ca33ffcbdec2fac18e21d1776f4f90fc03c25
Lucia <lu.morua22@gmail.com>
2024-05-31 23:37:11 -0300

    create button lucia

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
-import Button from "./components/ui/Button";
-
 function App() {
   return (
     <div className="bg-black min-h-screen grid place-content-center">
       <h1 className="text-white">Hello, world!</h1>
-      <Button nombre= "Bienvenido" className = "bg-blue-500 text-white"/>;
-      <Button nombre= "Enviado" className = "bg-blue-500 text-white"/>;
+     
     </div>
     
 
