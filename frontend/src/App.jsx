8c58b9e2a63cb6fa7ecbda23b73e7bb9e9783260
Lucia <lu.morua22@gmail.com>
2024-05-31 23:33:16 -0300

    create one button component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,13 @@
-
-
-import CardHeders from "./components/cardHeders";
-
-function App() {
-  return (
-    <>
-      <CardHeders />
-    </>
-
-
-import Button from "./Button";
-
-import Boton from "./components/boton";
-import P from "./components/ui/p";
-
+import Button from "./components/ui/Button";
 
 function App() {
   return (
     <div className="bg-black min-h-screen grid place-content-center">
       <h1 className="text-white">Hello, world!</h1>
-
-      <Button name="enviar" className="bg-primary text-white"/>
-      <Button name="comprar" className="bg-red-500 text-white"/>
-
-      <div>
-        <Boton name="abrir" />
-      </div>
-      <P name="hamburguesa con descuento"/>
- 
+      <Button nombre= "Bienvenido" className = "bg-blue-500 text-white"/>;
+      <Button nombre= "Enviado" className = "bg-blue-500 text-white"/>;
     </div>
+    
 
   );
 }
