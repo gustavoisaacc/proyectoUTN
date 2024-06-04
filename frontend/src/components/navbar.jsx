928c8d1d24a54108a1c8c5aa3caffbd52adc4090
RacedoSebastian <racedosebas@gmail.com>
2024-06-04 17:53:29 -0300

    nadvar subido

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,18 +1,14 @@
+import "../utils/navegacion"
+import { navegacion } from "../utils/navegacion"
 const Navbar = () => {
     return (
-        <nav className="navbar flex bg-primary justify-between align-center">
-            <div className="nav-logo s-20 m-20  bg-white " ></div>
-            <div className="nav-items a m-20 bg-white position-relative decoration-none">
-                <a href="">HOME</a>
-                <a href="">CONTACTO</a>
-                <a href="">INFORMACION</a>
-                <a href="">UBICACION</a>
-            </div>
-            <div className="nav-toggle none">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
+        <nav className="px-5 py-3 flex justify-between">
+            <div className="">burger rey</div>
+            <ul className="flex gap-5 text-white">
+               { navegacion.map(item => { 
+                return <li>{item.name}</li>
+                })}
+            </ul>
         </nav>
     )
 }
--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/footer";
-import CardHeders from "../components/cardHeders";
+import Navbar from "../components/navbar";
 
 
 function Layout() {
   return (
     <div className="w-screen h-screen grid grid-cols-layout grid-rows-[auto,1fr,auto] ">
       <header className="bg-primary row-span-1">
-        <CardHeders/>
+        <Navbar/>
       </header>
       <main className="">
         <Outlet />
