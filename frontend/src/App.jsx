080c19408a261aa01048734ea7d834e8568ef257
gustavoisaacc <gustavochaile7@gmail.com>
2024-06-03 22:08:59 -0300

    mergeado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,6 @@ function App() {
   return (
     <div className="bg-black min-h-screen grid place-content-center">
       <h1 className="text-white">Hello, world!</h1>
-<<<<<<< HEAD
-
-      <Botton name="bienvenido" className="bg-blue-500 text-white" />
-      <Botton name="enviado" className="text-blue-500" />
-=======
->>>>>>> f8d6f0e (add schemas and route product-category)
     </div>
   );
 }
