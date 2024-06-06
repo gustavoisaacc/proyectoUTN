7588239c6e5e2f510f567e87c2760d5013904980
RacedoSebastian <racedosebas@gmail.com>
2024-06-06 15:11:10 -0300

    menu hammburguesa realizado

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/footer";
-import Navbar from "../components/navbar";
+import Navbar from "../components/menuBurger/navbar";
 
 
 function Layout() {
