2d2aa683ba4a6d50fd804f49fa61e02f1ec31b5b
gustavoisaacc <gustavochaile7@gmail.com>
2024-06-11 07:15:01 -0300

    change in environment variable

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { app } from "./app.js";
+import { PORT } from "./config/config.js";
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
